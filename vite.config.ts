37c178d69437ff91bcd1e2f634e15daa0ef1d39b
Felipe Simis <felipee.oliveira17@hotmail.com>
2023-04-18 11:26:50 -0300

    Minor changes

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react';
 import { webfontDownload } from 'vite-plugin-webfont-dl';
 import eslintPlugin from '@nabla/vite-plugin-eslint';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
